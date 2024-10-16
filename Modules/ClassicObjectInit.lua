50e62f6ba3c3eb854e100bfa19f8da5c35485fc6
michaeljwalla <michaeljwalla@gmail.com>
2024-10-16 10:44:35 -0500

    added functionality when _ENV is present.

--- Modules/ClassicObjectInit.lua
+++ Modules/ClassicObjectInit.lua
@@ -1,6 +1,10 @@
 local defaults = {"Array", "LinkedList", "ListNode"}
 
 
+if (_ENV) then --getfenv doesn't exist?
+    _ENV.getfenv = function() return _ENV end
+end
+
 local module = {}
 
 module.import = function(arg) 
